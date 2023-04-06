ae37e9ebe677e804aa0b04cc4431947d2417d80e
htmldiana <dianekhomenko@gmail.com>
2023-04-06 20:52:18 +0200

    task-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import {Profile} from './Profile';
+import user from '../user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className='container'>
+      {user.map((user, idx) => (
+        <Profile key={idx} user={user} />
+      ))}
     </div>
   );
 };
