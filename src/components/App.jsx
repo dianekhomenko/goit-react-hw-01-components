e446368c73027ab50ba26d5a7b258bc4929ac7a3
htmldiana <dianekhomenko@gmail.com>
2023-04-11 17:39:20 +0200

    stats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import {Profile} from './Profile';
 import user from '../user.json';
+import { Statistics } from './Statistics';
+import statisticsData from '../data.json';
 
 export const App = () => {
   return (
-    <div className='container'>
+    <>
       {user.map((user, idx) => (
         <Profile key={idx} user={user} />
       ))}
-    </div>
+      <Statistics title="Upload stats" stats={statisticsData} />
+    </>
+
+    // <Statistics />
   );
 };
