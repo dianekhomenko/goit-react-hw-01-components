19bb2ef673ab4b84f3160d0728be140456c59919
htmldiana <dianekhomenko@gmail.com>
2023-04-12 18:28:30 +0200

    task-03

--- src/components/Friends/FriendItem/FriendItem.js
+++ src/components/Friends/FriendItem/FriendItem.js
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types';
 import css from 'components/Friends/FriendItem/FriendItem.module.css';
+import { MdCircle } from 'react-icons/md';
 
 export const FriendItem = ({ friend: { avatar, name, isOnline } }) => {
   return (
     <li className={css.item}>
       {isOnline ? (
-        <span className={css.online}></span>
+        <span className={css.online}>
+          <MdCircle />
+        </span>
       ) : (
-        <span className={css.offline}></span>
+        <span className={css.offline}>
+          <MdCircle />
+        </span>
       )}
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
